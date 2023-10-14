ca34255e927e317997dac3d8f6fb07ddb19605b5
emilyjiji <92887308+emilyjiji@users.noreply.github.com>
2023-10-14 12:52:23 -0400

    fixed minor error

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,6 +8,15 @@ const Intro = () => {
     const [major, setMajor] = useState("");
     const [interest, setInterest] = useState("");
 
+    const displayState = (e) => {
+        e.preventDefault();
+
+        console.log("Name:", name);
+        console.log("Email:", email);
+        console.log("Major:", major);
+        console.log("Interest:", interest);
+      };
+
     return (
         <div className="container">
           <h1>Basic Form</h1>
@@ -16,15 +25,15 @@ const Intro = () => {
             <input type="text" id="name" name="name" placeholder="Enter your name" onChange={(e) => setName(e.target.value)}/>
     
             <label htmlFor="email">Email:</label>
-            <input type="text" id="email" name="email" placeholder="Enter your email" />
+            <input type="text" id="email" name="email" placeholder="Enter your email" onChange={(e) => setEmail(e.target.value)}/>
     
             <label htmlFor="major">Major:</label>
-            <input type="text" id="major" name="major" placeholder="Enter your major" />
+            <input type="text" id="major" name="major" placeholder="Enter your major" onChange={(e) => setMajor(e.target.value)}/>
     
             <label htmlFor="interest">Interest:</label>
-            <input type="text" id="interest" name="interest" placeholder="Enter your interest" />
+            <input type="text" id="interest" name="interest" placeholder="Enter your interest" onChange={(e) => setInterest(e.target.value)}/>
     
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Submit" onClick={displayState} />
           </form>
         </div>
       );
