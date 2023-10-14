7176ddc5a93ded6cf7796e038c852dced429c466
emilyjiji <92887308+emilyjiji@users.noreply.github.com>
2023-10-14 12:35:56 -0400

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Intro from './components/Intro';
+import Main from './components/Main';
+
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <Router>
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="/main" element={<Main />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Intro.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Intro = () => {
+    return (
+        <div className="intro">
+            <h1>Intro</h1>
+        </div>
+    );
+}
+
+export default Intro;
