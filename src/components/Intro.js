8628d7be809153b391aae6f25f5fa32669a06bb1
emilyjiji <92887308+emilyjiji@users.noreply.github.com>
2023-10-14 20:06:31 -0400

    intro page ui/x complete!!

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Intro = () => {
   const [name, setName] = useState("");
@@ -19,36 +19,36 @@ const Intro = () => {
       interest: interest,
     };
 
-  try {
-    const response = await fetch("http://localhost:8000/create/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
+    try {
+      const response = await fetch("http://localhost:8000/create/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-    if (response.ok) {
-      const result = await response.json();
-      console.log("Data successfully sent to the server:", result);
-    } else {
-      console.error("Failed to send data to the server.");
+      if (response.ok) {
+        const result = await response.json();
+        console.log("Data successfully sent to the server:", result);
+      } else {
+        console.error("Failed to send data to the server.");
+      }
+      navigate("/main");
+    } catch (error) {
+      console.error("An error occurred:", error);
     }
-    navigate("/main");
-  } catch (error) {
-    console.error("An error occurred:", error);
-  }
-};
+  };
 
   return (
-    <div className="container d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-      <div className="text-center p-3" style={{ margin: "0 20px" }}> {/* Added margin to left container */}
-        <h1 className="mt-5">Basic Form</h1>
-        <p>
+    <div style={{ width: "95%", margin: "0 auto", minHeight: "100vh", backgroundColor: "#E8FFED"}} className="d-flex align-items-center justify-content-center" >
+      <div className="p-3" style={{ margin: "0 50px", padding: "20px" }}>
+        <h1 className="mt-5" style={{ fontSize: "50px", marginBottom: "35px", fontFamily: 'Jaques Francois', textShadow: '1px 1px 1px #000'}}>Welcome to AdapTree!</h1>
+        <p style={{ fontSize: "23px", marginBottom: "60px", fontFamily: 'Jaques Francois'}}>
           Welcome to our website. Please fill out the following form to get started.
         </p>
         <form onSubmit={handleSubmit}>
-          <div className="mb-3">
+          <div className="mb-4">
             <label htmlFor="name" className="form-label">Name:</label>
             <input
               type="text"
@@ -56,11 +56,12 @@ const Intro = () => {
               name="name"
               placeholder="Enter your name"
               className="form-control"
+              style={{ borderRadius: "25px", borderColor:"black" }}
               onChange={(e) => setName(e.target.value)}
             />
           </div>
 
-          <div className="mb-3">
+          <div className="mb-4">
             <label htmlFor="email" className="form-label">Email:</label>
             <input
               type="text"
@@ -68,11 +69,12 @@ const Intro = () => {
               name="email"
               placeholder="Enter your email"
               className="form-control"
+              style={{ borderRadius: "25px", borderColor:"black" }}
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
 
-          <div className="mb-3">
+          <div className="mb-4">
             <label htmlFor="major" className="form-label">Major:</label>
             <input
               type="text"
@@ -80,11 +82,12 @@ const Intro = () => {
               name="major"
               placeholder="Enter your major"
               className="form-control"
+              style={{ borderRadius: "25px", borderColor:"black" }}
               onChange={(e) => setMajor(e.target.value)}
             />
           </div>
 
-          <div className="mb-3">
+          <div className="mb-4">
             <label htmlFor="interest" className="form-label">Interest:</label>
             <input
               type="text"
@@ -92,19 +95,29 @@ const Intro = () => {
               name="interest"
               placeholder="Enter your interest"
               className="form-control"
+              style={{ borderRadius: "25px", borderColor:"black" }}
               onChange={(e) => setInterest(e.target.value)}
             />
           </div>
 
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <button 
+          type="submit"
+           className="btn btn-primary mb-2 mt-3"
+           style={{
+            backgroundColor: "#014328",
+            color: "white",
+            borderRadius: "66px",
+            padding: "8px 20px",
+           }}
+           >Submit</button>
+
         </form>
       </div>
-      <div className="text-center mt-5" style={{ margin: "0 20px" }}> {/* Added margin to right container */}
+      <div className="mt-5" style={{ margin: "0 50px" }}>
         <img
-          src="https://via.placeholder.com/400" // Increase the image size as needed
-          alt="Dummy Image"
-          className="img-fluid mb-5" // Increased margin bottom
-        />
+          src={process.env.PUBLIC_URL + '/images/nodeimage.png'}
+          alt="Node Image"
+          style={{ width: "100%", height: "auto" }} />
       </div>
     </div>
   );
